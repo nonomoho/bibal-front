83cfc18dc4d31bcb766abf012dc75f3ebbb47c37
Thomas Schersach <toj315@gmail.com>
2017-10-12 12:39:09 +0200

    Updated emprunt

--- src/components/emprunts/Update.vue
+++ src/components/emprunts/Update.vue
@@ -1,20 +1,28 @@
 <template>
   <section class="section">
-    <h1 class="title">Modifier un exemplaire</h1>
+    <h1 class="title">Ajouter un emprunt</h1>
     <form @submit.prevent="submit">
-      <div class="field is-expanded">
-        <label class="label">État</label>
-        <div class="select is-fullwidth">
-          <select v-model="exemplaire.etat">
-            <option v-for="etat in etats" :value="etat.id">{{ etat.name }}</option>
-          </select>
+      <div class="field">
+        <div class="control">
+          <label class="label">Date emprunt</label>
+          <date-picker v-model="emprunt.dateEmprunt"></date-picker>
         </div>
       </div>
       <div class="field is-expanded">
         <label class="label">Oeuvre</label>
         <div class="select is-fullwidth">
-          <select v-model="exemplaire.oeuvre.id">
+          <select v-model="emprunt.oeuvreId">
             <option v-for="oeuvre in oeuvres" :value="oeuvre.id">{{ oeuvre.titre }}</option>
+          </select>
+        </div>
+      </div>
+      <div class="field is-expanded">
+        <label class="label">Usager</label>
+        <div class="select is-fullwidth">
+          <select v-model="emprunt.usagerId">
+            <option v-for="usager in usagers" :value="usager.id">{{ usager.nom }} {{ usager.prenom
+              }}
+            </option>
           </select>
         </div>
       </div>
@@ -25,7 +33,8 @@
       </div>
       <div class="field is-grouped is-grouped-centered">
         <div class="control">
-          <router-link class="button is-outlined" :to="{name : 'exemplaires.index'}">Annuler</router-link>
+          <router-link class="button is-outlined" :to="{name : 'emprunts.index'}">Annuler
+          </router-link>
         </div>
       </div>
     </form>
@@ -38,49 +47,70 @@
   export default {
     props: ['id'],
     created () {
-      axios.get(`http://localhost:8083/exemplaires/${this.id}`)
-        .then(resp => {
-          this.exemplaire = {etat: resp.data.etat, ...resp.data._embedded}
-        })
+      axios.get(`http://localhost:8083/emprunts/${this.id}`)
+      .then(resp => {
+        const emprunt = resp.data
+        const embed = resp.data._embedded
+        this.emprunt = {
+          dateEmprunt: emprunt.dateEmprunt,
+          oeuvreId: embed.exemplaire.oeuvre.id,
+          usagerId: embed.usager.id
+        }
+      })
+
       axios.get('http://localhost:8083/oeuvres')
-        .then(resp => {
-          this.oeuvres = resp.data._embedded.oeuvres
+      .then(resp => {
+        this.oeuvres = resp.data._embedded.oeuvres
+      })
+      .catch(e => {
+        this.$toast.open({
+          message: 'Erreur',
+          type: 'is-danger'
         })
-        .catch(e => {
-          this.$toast.open({
-            message: 'Erreur',
-            type: 'is-danger'
-          })
+      })
+
+      axios.get('http://localhost:8083/usagers')
+      .then(resp => {
+        this.usagers = resp.data._embedded.usagers
+      })
+      .catch(e => {
+        this.$toast.open({
+          message: 'Erreur',
+          type: 'is-danger'
         })
+      })
     },
     data () {
       return {
-        exemplaire: {oeuvre: {}},
+        emprunt: {
+          oeuvreId: ''
+        },
         etats: [
-          {id: 0, name: 'Bon'},
-          {id: 1, name: 'Abimé'},
-          {id: 2, name: 'Inutilisable'}
+          {id: 'BON', name: 'Bon'},
+          {id: 'ABIME', name: 'Abimé'},
+          {id: 'INUTILISABLE', name: 'Inutilisable'}
         ],
-        oeuvres: []
+        oeuvres: [],
+        usagers: []
       }
     },
     methods: {
       submit () {
-        axios.put(`http://localhost:8083/exemplaires/${this.id}`, this.exemplaire)
-          .then(resp => {
-            this.$router.push({name: 'exemplaires.index'}, () => {
-              this.$toast.open({
-                message: 'Modification effectuée',
-                type: 'is-primary'
-              })
-            })
-          })
-          .catch(e => {
+        axios.post('http://localhost:8083/emprunts', this.emprunt)
+        .then(resp => {
+          this.$router.push({name: 'emprunts.index'}, () => {
             this.$toast.open({
-              message: 'Erreur',
-              type: 'is-danger'
+              message: 'Emprunt enregistré',
+              type: 'is-primary'
             })
           })
+        })
+        .catch(e => {
+          this.$toast.open({
+            message: `Erreur : ${e.response.data}`,
+            type: 'is-danger'
+          })
+        })
       }
     }
   }
